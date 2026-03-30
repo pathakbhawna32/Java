5bd234f6b36bc457adc00ca367436242a356e354
pathakbhawna32 <pathakbhawna32@gmail.com>
2026-03-30 21:24:59 +0530

    Create LambdaExpressions.java

---
+++ LambdaExpressions.java
@@ -0,0 +1,11 @@
+interface Add{
+	int addition(int a, int b);
+}
+public class lamda1 {
+
+	public static void main(String[] args) {
+		Add add = (a,b) -> a+b;
+		int result = add.addition(10, 20);
+	   System.out.println("Sum: " + result);
+				}
+	}
